37cc1e909935bfadbc044910d5b118e89a6610a6
Rushion <rushionchegge@gmail.com>
2024-07-22 11:59:02 +0300

    js file changes text on the page when DOM content has loaded

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code goes here
+// Initial setup when the DOM is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  const pElement = document.querySelector("p");
+
+  if (pElement) {
+    pElement.textContent = "This is really cool!";
+  }
+});
+
+window.onload = function () {
+  const pElement = document.querySelector("p");
+
+  if (!pElement) {
+    const newP = document.createElement("p");
+    newP.textContent =
+      "JavaScript is so cool. It lets me add text to my page programmatically.";
+    document.body.appendChild(newP);
+  } else {
+    pElement.textContent =
+      "JavaScript is so cool. It lets me add text to my page programmatically.";
+  }
+};
